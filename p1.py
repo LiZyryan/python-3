1c914f895e32f18905146db343bef0c15259659c
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-10-04 09:51:33 +0300

    Add files via upload

--- p1.py
+++ p1.py
@@ -1,11 +1,5 @@
-def sum_even_indexes_and_multiply(lst):
-    if not lst:
-        return 0
-    even_index_sum = sum(lst[::2])
-    return even_index_sum * lst[-1]
-
-# Перевірка прикладів
-print(sum_even_indexes_and_multiply([0, 1, 7, 2, 4, 8]))  
-print(sum_even_indexes_and_multiply([1, 3, 5]))
-print(sum_even_indexes_and_multiply([6]))
-print(sum_even_indexes_and_multiply([]))
+import random
+random_list = [random.randint(1, 100) for _ in range(random.randint(3, 10))]
+result_list = [random_list[0], random_list[2], random_list[-2]]
+print("Початковий список:", random_list)
+print("Новий список:", result_list)
