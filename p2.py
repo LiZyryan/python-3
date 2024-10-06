06e3dc9c39bf5173fc1c5c0662fc6f8ab1e3cacd
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-10-06 13:21:36 +0300

    Add files via upload

--- p2.py
+++ p2.py
@@ -1,21 +1,8 @@
 import string
-import keyword
-
-def is_valid_variable_name(name):
-    if name in keyword.kwlist:
-        return False
-    if name.count('_') > 1:
-        return False
-    if name[0].isdigit():
-        return False
-    if any(char.isupper() for char in name):
-        return False
-    if any(char in string.punctuation.replace('_', '') for char in name) or ' ' in name:
-        return False
-    return True
-while True:
-    user_input = input("Введіть ім'я змінної (або 'exit' для виходу): ")
-    if user_input.lower() == 'exit':
-        break
-    is_valid = is_valid_variable_name(user_input)
-    print(is_valid)
+def create_hashtag(input_string):
+    input_string = input_string.translate(str.maketrans('', '', string.punctuation))
+    words = input_string.split()
+    hashtag = '#' + ''.join(word.capitalize() for word in words)
+    return hashtag[:140]
+user_input = input("Введіть рядок: ")
+print(create_hashtag(user_input))
