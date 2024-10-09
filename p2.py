0cae387908346c1f7f568dd7c46cb91bbb047688
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-10-09 11:33:44 +0300

    Add files via upload

--- p2.py
+++ p2.py
@@ -1,8 +1,27 @@
-import string
-def create_hashtag(input_string):
-    input_string = input_string.translate(str.maketrans('', '', string.punctuation))
-    words = input_string.split()
-    hashtag = '#' + ''.join(word.capitalize() for word in words)
-    return hashtag[:140]
-user_input = input("Введіть рядок: ")
-print(create_hashtag(user_input))
+def convert_seconds(seconds):
+    if seconds < 0 or seconds >= 8640000:
+        return "Число повинно бути більше або дорівнювати 0 і менше ніж 8640000."
+
+    days, remainder = divmod(seconds, 86400)
+    hours, remainder = divmod(remainder, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    if days == 1:
+        day_word = "день"
+    elif 2 <= days <= 4:
+        day_word = "дні"
+    else:
+        day_word = "днів"
+    time_str = f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
+
+    return f"{days} {day_word}, {time_str}"
+
+print(convert_seconds(0))
+print(convert_seconds(224930))
+print(convert_seconds(466289))
+print(convert_seconds(950400))
+print(convert_seconds(1209600))
+print(convert_seconds(1900800))
+print(convert_seconds(8639999))
+print(convert_seconds(22493))
+print(convert_seconds(7948799))
