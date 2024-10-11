b7541df8bc1800ca22001363ba7bc51e465da992
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-10-11 17:19:11 +0300

    Add files via upload

--- p2.py
+++ p2.py
@@ -1,27 +1,7 @@
-def convert_seconds(seconds):
-    if seconds < 0 or seconds >= 8640000:
-        return "Число повинно бути більше або дорівнювати 0 і менше ніж 8640000."
+def say_hi(name, age):
+    return f"Hi. My name is {name} and I'm {age} years old"
 
-    days, remainder = divmod(seconds, 86400)
-    hours, remainder = divmod(remainder, 3600)
-    minutes, seconds = divmod(remainder, 60)
-
-    if days == 1:
-        day_word = "день"
-    elif 2 <= days <= 4:
-        day_word = "дні"
-    else:
-        day_word = "днів"
-    time_str = f"{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
-
-    return f"{days} {day_word}, {time_str}"
-
-print(convert_seconds(0))
-print(convert_seconds(224930))
-print(convert_seconds(466289))
-print(convert_seconds(950400))
-print(convert_seconds(1209600))
-print(convert_seconds(1900800))
-print(convert_seconds(8639999))
-print(convert_seconds(22493))
-print(convert_seconds(7948799))
+# Тестування функції
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print('ОК')
