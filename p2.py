ac246d40279a01b144f9cb3a831f798c20bb41c7
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-10-13 12:23:23 +0300

    Add files via upload

--- p2.py
+++ p2.py
@@ -1,7 +1,13 @@
-def say_hi(name, age):
-    return f"Hi. My name is {name} and I'm {age} years old"
-
-# Тестування функції
-assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
-assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+def second_index(text, some_str):
+    first_index = text.find(some_str)
+    if first_index == -1:
+        return None
+    second_index = text.find(some_str, first_index + 1)
+    if second_index == -1:
+        return None
+    return second_index
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
 print('ОК')
