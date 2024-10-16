a4302594d91295628f27ed3f65845f20f2cf8c5c
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-10-16 13:45:56 +0300

    p1.py

--- p1.py
+++ p1.py
@@ -1,5 +1,9 @@
-import random
-random_list = [random.randint(1, 100) for _ in range(random.randint(3, 10))]
-result_list = [random_list[0], random_list[2], random_list[-2]]
-print("Початковий список:", random_list)
-print("Новий список:", result_list)
+def add_one(some_list):
+    number = int("".join(map(str, some_list)))
+    number += 1
+    return [int(digit) for digit in str(number)]
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+assert add_one([0]) == [1], 'Test3'
+assert add_one([9]) == [1, 0], 'Test4'
+print("ОК")
