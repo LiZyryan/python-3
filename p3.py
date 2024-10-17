9263058604fc06a53b8949655512ff93dc68f888
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-10-17 10:10:44 +0300

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,7 +1,9 @@
-def common_elements():
-    set1 = {i for i in range(100) if i % 3 == 0}
-    set2 = {i for i in range(100) if i % 5 == 0}
-    intersection_set = set1.intersection(set2)
-    return intersection_set
+def find_unique_value(some_list):
+    for num in some_list:
+        if some_list.count(num) == 1:
+            return num
+assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
 
-assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
+print("ОК")
