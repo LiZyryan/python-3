063a8dc0177aa61259bc66187c9357d1bd19d945
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-10-20 11:29:51 +0300

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,9 +1,9 @@
-def add_one(some_list):
-    number = int("".join(map(str, some_list)))
-    number += 1
-    return [int(digit) for digit in str(number)]
-assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
-assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
-assert add_one([0]) == [1], 'Test3'
-assert add_one([9]) == [1, 0], 'Test4'
-print("ОК")
+def popular_words(text, words):
+    word_list = text.lower().split()
+    word_count = {word: 0 for word in words}
+    for word in word_list:
+        if word in word_count:
+            word_count[word] += 1
+    return word_count
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
+print('OK')
