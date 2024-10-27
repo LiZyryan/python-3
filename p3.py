5fc4dceb29fa4270324e80b90c8c1c816012abd1
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-10-27 16:12:52 +0200

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,9 +1,6 @@
-def find_unique_value(some_list):
-    for num in some_list:
-        if some_list.count(num) == 1:
-            return num
-assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
-assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
-assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
-
-print("ОК")
+def is_even(digit):
+    return digit % 2 == 0
+assert is_even(2) == True, 'Test1'
+assert is_even(5) == False, 'Test2'
+assert is_even(0) == True, 'Test3'
+print('OK')
