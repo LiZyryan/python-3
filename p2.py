843b3f5872f092b65c0b1782c014da57ee729c23
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-10-27 16:11:56 +0200

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,9 +1,13 @@
-def difference(*args):
-    if not args:
-        return 0
-    return round(max(args) - min(args), 2)
-assert difference(1, 2, 3) == 2, 'Test1'
-assert difference(5, -5) == 10, 'Test2'
-assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4, 'Test3'
-assert difference() == 0, 'Test4'
+import re
+
+def first_word(text):
+    match = re.search(r"[a-zA-Z']+", text)
+    return match.group(0) if match else ""
+
+assert first_word("Hello world") == "Hello", 'Test1'
+assert first_word("greetings, friends") == "greetings", 'Test2'
+assert first_word("don't touch it") == "don't", 'Test3'
+assert first_word(".., and so on ...") == "and", 'Test4'
+assert first_word("hi") == "hi", 'Test5'
+assert first_word("Hello.World") == "Hello", 'Test6'
 print('OK')
