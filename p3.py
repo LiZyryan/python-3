dde69ae5ada415230298d625ff23858daf321bd2
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-10-30 20:56:07 +0200

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,6 +1,6 @@
-def is_even(digit):
-    return digit % 2 == 0
-assert is_even(2) == True, 'Test1'
-assert is_even(5) == False, 'Test2'
-assert is_even(0) == True, 'Test3'
-print('OK')
+def is_even(number):
+    return (number & 1) == 0
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
+print('Ok')
