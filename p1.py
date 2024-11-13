71e7607c547bc3b5669e8bc8a8dfc29b1805c294
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-11-13 16:29:11 +0200

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,79 +1,42 @@
-class GroupLimitError(Exception):
-    def __init__(self, message="Досягнуто максимальну кількість студентів у групі (10)."):
-        super().__init__(message)
-
-class Human:
-    def __init__(self, gender, age, first_name, last_name):
-        self.gender = gender
-        self.age = age
-        self.first_name = first_name
-        self.last_name = last_name
-
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def get_square(self):
+        return self.width * self.height
+    def __eq__(self, other):
+        if isinstance(other, Rectangle):
+            return self.get_square() == other.get_square()
+        return NotImplemented
+    def __add__(self, other):
+        if isinstance(other, Rectangle):
+            total_area = self.get_square() + other.get_square()
+            new_width, new_height = self._find_dimensions_for_area(total_area)
+            return Rectangle(new_width, new_height)
+        return NotImplemented
+    def __mul__(self, n):
+        if isinstance(n, (int, float)) and n > 0:
+            new_area = self.get_square() * n
+            new_width, new_height = self._find_dimensions_for_area(new_area)
+            return Rectangle(new_width, new_height)
+        return NotImplemented
+    def _find_dimensions_for_area(self, area):
+        side = int(area ** 0.5)
+        while area % side != 0:
+            side -= 1
+        return side, area // side
     def __str__(self):
-        return f"{self.first_name} {self.last_name}, {self.age} years old, {self.gender}"
-
-class Student(Human):
-    def __init__(self, gender, age, first_name, last_name, record_book):
-        super().__init__(gender, age, first_name, last_name)
-        self.record_book = record_book
-
-    def __str__(self):
-        return f"{super().__str__()}, Record Book: {self.record_book}"
-
-class Group:
-    MAX_STUDENTS = 10
-
-    def __init__(self, number):
-        self.number = number
-        self.group = set()
-
-    def add_student(self, student):
-        if len(self.group) >= Group.MAX_STUDENTS:
-            raise GroupLimitError()
-        self.group.add(student)
-
-    def find_student(self, last_name):
-        for student in self.group:
-            if student.last_name == last_name:
-                return student
-        return None
-
-    def delete_student(self, last_name):
-        student = self.find_student(last_name)
-        if student:
-            self.group.remove(student)
-
-    def __str__(self):
-        all_students = "\n".join(str(student) for student in self.group)
-        return f"Number: {self.number}\n{all_students}"
-
-st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
-st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
-st3 = Student('Male', 22, 'John', 'Doe', 'AN146')
-st4 = Student('Female', 23, 'Jane', 'Smith', 'AN147')
-st5 = Student('Male', 27, 'Michael', 'Brown', 'AN148')
-st6 = Student('Female', 21, 'Anna', 'White', 'AN149')
-st7 = Student('Male', 24, 'Chris', 'Davis', 'AN150')
-st8 = Student('Female', 22, 'Karen', 'Johnson', 'AN151')
-st9 = Student('Male', 26, 'James', 'Wilson', 'AN152')
-st10 = Student('Female', 28, 'Emily', 'Clark', 'AN153')
-st11 = Student('Male', 20, 'Tom', 'Anderson', 'AN154')
-
-gr = Group('PD1')
-
-for student in [st1, st2, st3, st4, st5, st6, st7, st8, st9, st10]:
-    gr.add_student(student)
-print(gr)
-
-try:
-    gr.add_student(st11)
-except GroupLimitError as e:
-    print(e)
-assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
-assert gr.find_student('Jobs2') is None, 'Test2'
-assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод пошуку повинен повертати екземпляр'
-
-gr.delete_student('Taylor')
-print(gr)
-
-gr.delete_student('Taylor')
+        return f"Rectangle(width={self.width}, height={self.height}, area={self.get_square()})"
+r1 = Rectangle(2, 4)
+r2 = Rectangle(3, 6)
+assert r1.get_square() == 8, 'Test1'
+assert r2.get_square() == 18, 'Test2'
+r3 = r1 + r2
+assert r3.get_square() == 26, 'Test3'
+r4 = r1 * 4
+assert r4.get_square() == 32, 'Test4'
+assert Rectangle(3, 6) == Rectangle(2, 9), 'Test5'
+print(r1)
+print(r2)
+print(r3)
+print(r4)
