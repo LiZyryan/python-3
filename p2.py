f455ea700e190562db1d78c6e7d538bdfd92aabf
LiZyryan <164018820+LiZyryan@users.noreply.github.com>
2024-11-15 10:27:31 +0200

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,52 +1,35 @@
-class Item:
-    def __init__(self, name, price, description, dimensions):
-        self.name = name
-        self.price = price
-        self.description = description
-        self.dimensions = dimensions
+class Fraction:
+    def __init__(self, a, b):
+        if b == 0:
+            raise ValueError("Denominator cannot be zero")
+        self.a = a
+        self.b = b
+    def __mul__(self, other):
+        return Fraction(self.a * other.a, self.b * other.b)
+    def __add__(self, other):
+        return Fraction(self.a * other.b + other.a * self.b, self.b * other.b)
+    def __sub__(self, other):
+        return Fraction(self.a * other.b - other.a * self.b, self.b * other.b)
+    def __eq__(self, other):
+        return self.a * other.b == self.b * other.a
+    def __gt__(self, other):
+        return self.a * other.b > self.b * other.a
+    def __lt__(self, other):
+        return self.a * other.b < self.b * other.a
     def __str__(self):
-        return f"{self.name}, price: {self.price}, description: {self.description}, dimensions: {self.dimensions}"
-
-class User:
-    def __init__(self, name, surname, numberphone):
-        self.name = name
-        self.surname = surname
-        self.numberphone = numberphone
-    def __str__(self):
-        return f"{self.name} {self.surname}"
-
-class Purchase:
-    def __init__(self, user):
-        self.user = user
-        self.products = {}
-    def add_item(self, item, cnt):
-        if item in self.products:
-            self.products[item] += cnt
-        else:
-            self.products[item] = cnt
-    def get_total(self):
-        return sum(item.price * cnt for item, cnt in self.products.items())
-    def __str__(self):
-        items_str = "\n".join(f"{item.name}: {cnt} pcs." for item, cnt in self.products.items())
-        return f"User: {self.user}\nItems:\n{items_str}"
-
-
-lemon = Item('lemon', 5, "yellow", "small")
-apple = Item('apple', 2, "red", "middle")
-print(lemon)
-print(apple)
-
-buyer = User("Ivan", "Ivanov", "02628162")
-print(buyer)
-cart = Purchase(buyer)
-cart.add_item(lemon, 4)
-cart.add_item(apple, 20)
-
-print(cart)
-assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
-assert cart.get_total() == 60, "Всього 60"
-assert cart.get_total() == 60, 'Повинно залишатися 60!'
-
-cart.add_item(apple, 10)
-print(cart)
-assert cart.get_total() == 100, "Повинно бути 100!"
+        return f"Fraction: {self.a}, {self.b}"
+f_a = Fraction(2, 3)
+f_b = Fraction(3, 6)
+f_c = f_b + f_a
+assert str(f_c) == 'Fraction: 21, 18'
+f_d = f_b * f_a
+assert str(f_d) == 'Fraction: 6, 18'
+f_e = f_a - f_b
+assert str(f_e) == 'Fraction: 3, 18'
+assert f_d < f_c  # True
+assert f_d > f_e  # True
+assert f_a != f_b  # True
+f_1 = Fraction(2, 4)
+f_2 = Fraction(3, 6)
+assert f_1 == f_2  # True
+print('OK')
